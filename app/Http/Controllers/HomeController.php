1d2697df42fabf4bbd084d55e90422b2d20ac8fb
czarframe <cesaragstincs@gmail.com>
2022-10-14 11:42:35 -0300

    Configurando DB e Produto Controller (Listar, novo e cadastro)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -19,12 +20,7 @@ class HomeController extends Controller
     }
 
     public function users() {
-        $users = [
-            ['nome' => 'José da Silva', 'idade'  => 64, 'cidade' => 'Maringá'],
-            ['nome' => 'Maria Joana', 'idade' => 26, 'cidade' => 'Londrina'],
-            ['nome' => 'Ana Carolina', 'idade' => 35, 'cidade' => 'Curitiba'],
-            ['nome' => 'Ana Carolina', 'idade' => 35, 'cidade' => 'Curitiba']
-        ];
+        $users = DB::table('usuarios')->get();
 
         return view('users', [
             'usuarios' => $users
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
 @@ -13,6 +14,8 @@
 |
 */
+
+// CRUD
+Route::get('produtos', [ProdutosController::class, 'index']);
+Route::get('produtos/novo', [ProdutosController::class, 'create']);
+Route::post('produtos/salvar', [ProdutosController::class, 'store']);
+Route::get('produtos/{id}', [ProdutosController::class, 'show']);
 
 Route::get('home', [HomeController::class, 'index']);
 Route::get('users', [HomeController::class, 'users']);
